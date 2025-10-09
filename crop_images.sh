48e4bc50916c1a1d8f85cc27f84dfea72537fc9d
Sean Thawe <sean.thawe@outlook.com>
2025-10-09 18:25:27 +0200

    feat: Add configurable output directory and improved logging

--- crop_images.sh
+++ crop_images.sh
@@ -3,6 +3,7 @@
 # Default values
 LANDSCAPE_DIMS="480x230"
 PORTRAIT_DIMS="480x600"
+OUTPUT_DIR="cropped_images"
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -15,9 +16,13 @@ while [[ $# -gt 0 ]]; do
             PORTRAIT_DIMS="$2"
             shift; shift
             ;;
+        -o|--output)
+            OUTPUT_DIR="$2"
+            shift; shift
+            ;;
         *)
             echo "Unknown option: $1"
-            echo "Usage: $0 [-l WxH] [-p WxH]"
+            echo "Usage: $0 [-l WxH] [-p WxH] [-o output_dir]"
             exit 1
             ;;
     esac
@@ -29,10 +34,13 @@ LANDSCAPE_H=${LANDSCAPE_DIMS#*x}
 PORTRAIT_W=${PORTRAIT_DIMS%x*}
 PORTRAIT_H=${PORTRAIT_DIMS#*x}
 
-echo "Landscape: ${LANDSCAPE_W}x${LANDSCAPE_H}"
-echo "Portrait:  ${PORTRAIT_W}x${PORTRAIT_H}"
+mkdir -p "$OUTPUT_DIR"
 
-# Process images (same logic as above)
+echo "Processing images..."
+echo "  Landscape: ${LANDSCAPE_W}x${LANDSCAPE_H}"
+echo "  Portrait:  ${PORTRAIT_W}x${PORTRAIT_H}"
+echo "  Output:    ./$OUTPUT_DIR/"
+
 for img in *.jpg *.jpeg *.png; do
     [ -e "$img" ] || continue
     
@@ -40,9 +48,17 @@ for img in *.jpg *.jpeg *.png; do
     width=$(echo "$dimensions" | cut -d',' -f1)
     height=$(echo "$dimensions" | cut -d',' -f2)
     
+    echo "Processing: $img (${width}x${height})"
+    
     if [ "$height" -gt "$width" ]; then
-        ffmpeg -i "$img" -vf "scale=${PORTRAIT_W}:-1, crop=${PORTRAIT_W}:${PORTRAIT_H}" "cropped_${img%.*}.jpg"
+        ffmpeg -i "$img" -vf "scale=${PORTRAIT_W}:-1, crop=${PORTRAIT_W}:${PORTRAIT_H}" -loglevel error "$OUTPUT_DIR/cropped_${img%.*}.jpg"
+        echo "  Cropped to portrait: ${PORTRAIT_W}x${PORTRAIT_H}"
     else
-        ffmpeg -i "$img" -vf "scale=${LANDSCAPE_W}:-1, crop=${LANDSCAPE_W}:${LANDSCAPE_H}" "cropped_${img%.*}.jpg"
+        ffmpeg -i "$img" -vf "scale=${LANDSCAPE_W}:-1, crop=${LANDSCAPE_W}:${LANDSCAPE_H}" -loglevel error "$OUTPUT_DIR/cropped_${img%.*}.jpg"
+        echo "  Cropped to landscape: ${LANDSCAPE_W}x${LANDSCAPE_H}"
     fi
+    echo "Done: $img"
+    echo
 done
+
+echo "All images saved to: ./$OUTPUT_DIR/"
