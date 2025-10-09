3c18f6ea955fcc59cf71242452dcfcbaed1a3648
Sean Thawe <sean.thawe@outlook.com>
2025-10-09 18:08:04 +0200

    feat: Add configurable dimensions via command-line arguments

--- crop_images.sh
+++ crop_images.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-for img in *.jpg; do
-    # Get image dimensions using ffprobe
+# Default values
+LANDSCAPE_DIMS="480x230"
+PORTRAIT_DIMS="480x600"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -l|--landscape)
+            LANDSCAPE_DIMS="$2"
+            shift; shift
+            ;;
+        -p|--portrait)
+            PORTRAIT_DIMS="$2"
+            shift; shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Usage: $0 [-l WxH] [-p WxH]"
+            exit 1
+            ;;
+    esac
+done
+
+# Extract dimensions
+LANDSCAPE_W=${LANDSCAPE_DIMS%x*}
+LANDSCAPE_H=${LANDSCAPE_DIMS#*x}
+PORTRAIT_W=${PORTRAIT_DIMS%x*}
+PORTRAIT_H=${PORTRAIT_DIMS#*x}
+
+echo "Landscape: ${LANDSCAPE_W}x${LANDSCAPE_H}"
+echo "Portrait:  ${PORTRAIT_W}x${PORTRAIT_H}"
+
+# Process images (same logic as above)
+for img in *.jpg *.jpeg *.png; do
+    [ -e "$img" ] || continue
+    
     dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 "$img")
     width=$(echo "$dimensions" | cut -d',' -f1)
     height=$(echo "$dimensions" | cut -d',' -f2)
     
-    echo "Processing: $img (${width}x${height})"
-    
-    # Determine orientation and apply appropriate crop
     if [ "$height" -gt "$width" ]; then
-        # Portrait orientation - crop to 480x600
-        echo "  Detected portrait orientation, cropping to 480x600"
-        ffmpeg -i "$img" -vf "scale=480:-1, crop=480:600" "cropped_${img}"
+        ffmpeg -i "$img" -vf "scale=${PORTRAIT_W}:-1, crop=${PORTRAIT_W}:${PORTRAIT_H}" "cropped_${img%.*}.jpg"
     else
-        # Landscape or square orientation - crop to 480x230
-        echo "  Detected landscape orientation, cropping to 480x230"
-        ffmpeg -i "$img" -vf "scale=480:-1, crop=480:230" "cropped_${img}"
+        ffmpeg -i "$img" -vf "scale=${LANDSCAPE_W}:-1, crop=${LANDSCAPE_W}:${LANDSCAPE_H}" "cropped_${img%.*}.jpg"
     fi
 done
-
-echo "All images processed!"
